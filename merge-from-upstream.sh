36809462e99f14c81a294b65d72f32318a3d6862
angelofdeauth <aj@angelofdeauth.host>
2019-06-22 17:31:56 -0500

    Added merge from upstream script -- will need to update as needed

---
+++ merge-from-upstream.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# @File: merge-from-upstream.sh
+# @Date: 2019-06-22 17:09:31
+# @OA:   antonioe
+# @CA:   antonioe
+# @Time: 2019-06-22 17:09:59
+# @Mail: antonioe@wolfram.com
+# @Copy: Copyright © 2019 Antonio Escalera <aj@angelofdeauth.host>
+
+git fetch upstream
+git merge upstream/master
